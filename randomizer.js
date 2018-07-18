5baa3b556e2aa3c39d12102e4125ead235e418c8
aaron.toth <aaron.toth@ontario.ca>
2018-07-17 22:52:48 -0400

    More functionality and alerts.

--- randomizer.js
+++ randomizer.js
@@ -2,11 +2,39 @@
 
 // Global vars
 var done="x";
-var number=1;
+var population=100;
+var computerDraw=20;
+var humanDraw=7;
+
+var computerNumbers=new Array();
+var humanNumbers=new Array();
 
 
-// This function will write to the continuous log window
-function randomize()
+// This function will generate some random data
+function randomizeComputer()
 {
-	alert("It's time to start randomizing!");
+	var x;
+	for(x=0; x<+computerDraw; x++)
+	{
+		computerNumbers[x]=(Math.round(Math.random()*99)+1);
+	}
+}
+
+function randomizeHuman()
+{
+	var x;
+	var num5=(Math.round(Math.random()*5)+1);
+	computerNumbers[0]=(Math.round(Math.random()*99)+1);
+
+	for(x=0; x<+humanDraw; x++)
+	{
+		humanNumbers[x]=(Math.round(Math.random()*99)+1);
+	}
+}
+
+function showMe()
+{
+	alert("It's time to start randomizing! \n" 
+	+ "Computer Numbers: " + computerNumbers.toString() + "\n"
+	+ "Human Numbers: " + humanNumbers.toString());
 }
