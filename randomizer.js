6c836a679f8cb03059075755a0f721159967745c
aaron.toth <aaron.toth@ontario.ca>
2018-07-17 22:14:59 -0400

    changed library to Zingchart.  Page is working with displaying charts and can receive data from external js file.

---
+++ randomizer.js
@@ -0,0 +1,12 @@
+// External JS file for randomizer.html
+
+// Global vars
+var done="x";
+var number=1;
+
+
+// This function will write to the continuous log window
+function randomize()
+{
+	alert("It's time to start randomizing!");
+}
