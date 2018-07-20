81df21da1cce30305867ab7ea997b98e8b3c0807
aaron.toth <aaron.toth@ontario.ca>
2018-07-20 12:59:28 -0400

    bug fixes and now graph generates the matchArray which holds the number of times you had the same numbers as the computer.  bug fixes, functionality and improvements in next commit.

--- randomizer.js
+++ randomizer.js
@@ -8,9 +8,9 @@ var humanDraw=10;
 
 var computerNumbers=new Array();
 var humanNumbers=new Array();
+var matchArray=[0,0,0,0,0,0,0,0,0,0,0];
 
-
-// This function will generate some random data
+//generates random data (for computer)
 function randomizeComputer()
 {
 	var x;
@@ -20,6 +20,7 @@ function randomizeComputer()
 	}
 }
 
+//generates random date for the player
 function randomizeHuman()
 {
 	var x;
@@ -29,25 +30,59 @@ function randomizeHuman()
 	}
 }
 
+//generates random date for both computer and player (using the chance library)
 function randomizePcHuman()
 {
-	computerNumbers = chance.unique(chance.natural, 20, {min: 1, max: 100});
-	humanNumbers = chance.unique(chance.natural, 10, {min: 1, max: 100});
+	computerNumbers = chance.unique(chance.natural, 20, {min: 0, max: 70});
+	humanNumbers = chance.unique(chance.natural, 10, {min: 0, max: 70});
 }
 
+//finds the number of matches between the computer's numbers and the player's numbers
+function findMatches()
+{
+	var x;
+	var y;
+	var numberOfMatches=0;
+	var indexToUpdate=0;
+
+	for(x=0; x<humanNumbers.length; x++)
+	{
+		for(y=0; y<computerNumbers.length; y++)
+		{
+			if(humanNumbers[x]==computerNumbers[y])
+			{
+				numberOfMatches++;
+				{ break; }
+			}
+		}
+	}
+	indexToUpdate=numberOfMatches;
+	matchArray[indexToUpdate] += 1;
+	alert("number of matches: " + numberOfMatches);
+}
+
+//alerts both the computer and player arrays
 function alertPcHuman()
 {
-	alert("It's time to start randomizing! \n" 
+	alert("It's time to start randomiiiiiiing! \n" 
 	+ "Computer Numbers: " + computerNumbers.toString() + "\n"
 	+ "Human Numbers: " + humanNumbers.toString());
 }
 
+//returns the computer's array to the GUI call
 function getComputer()
 {
 	return computerNumbers;
 }
 
+//returns the player's array to the GUI call
 function getHuman()
 {
 	return humanNumbers;
+}
+
+//returns the match array to the GUI call
+function getMatch()
+{
+	return matchArray;
 }
