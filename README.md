154d67cea78eb14105f505b53c5a3fb567aad873
aaron.toth <aaron.toth@ontario.ca>
2018-07-23 15:37:10 -0400

    bug fixes and more features.  now you can clear the data with a button click.  the 5 Ms are now calculated and working.  bug fixes, sd and sampling functionality in next commit.

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ This is a tool to help visualize randomess and probability by displaying data in
 * Chance - for generating the random data set for the bar charts
 * [Chance](https://chancejs.com/)
 
+### Installation Instructions
+1. copy files to a local folder
+2. download the JS libraries from the references above
+  1. if you don't want to download the JS libraries, just modify the JS 'script' references in the HTML to point to them externally
+3. open the index.html file in the browser of your choice
+4. HAVE FUN!!!
+ 
 ### How to use
 This tool is based off the game Keno.  I have preselected the game where you (as the player) pick 10 unique numbers and the computer (i.e. the OLG) picks 20 unique numbers.  We want to see how many you get the same per draw.  A 'draw' is one random generation of numbers for both the player and the computer.  
 
@@ -18,27 +25,29 @@ After a single play of the game, when you reload the graph, a single column in t
 
 The 'Multiple Draws' section will show you the data after playing the game a large number of times.
 
-### Installation Instructions
-1. copy files to a local folder
-2. download the JS libraries from the references above
-  1. if you don't want to download the JS libraries, just modify the JS 'script' references in the HTML to point to them externally
-3. open the index.html file in the browser of your choice
-4. HAVE FUN!!!
+When the page intially loads it has some data in the graph.  That is there just to give a nice little graphic to the user before starting.
+
+You can hover over each bar which will give you the count of how many times that happened.
+
 
 ### Single Draw
 * Randomize Both - generates both (computer and player) of your numbers in one call
 * Alert PC & Human - alert that shows the 2 sets of numbers
 
+
 ### Multiple Draws
 * '# of Draws' - the number of times to play the game between the player and the computer
 * Draw - draws random sets of numbers for '# of Draws' times for both the player and computer 
 
+
 ### Display
 * Reload - reloads the graph with the new data
+
 
 ### Odds
 These are the official odds of playing the official game through the OLG
 [Odds](https://lottery.olg.ca/en-ca/daily-games/daily-keno/daily-keno-odds-and-payouts#dailykeno-odds/)
+
 
 ### Notes
 * Randomiiiiiiizer uses a computer to generate the random set(s) of numbers.  In real life a bunch of balls are tossed around a machine and until 20 are sucked through a hole and observed.  These 2 distinct methods will have different results.
--- randomizer.js
+++ randomizer.js
@@ -1,22 +1,24 @@
 // External JS file for randomizer.html
 
 // Global vars
-var done="x";
-var population=70;
-var computerDraw=20;
-var humanDraw=10;
-var numberOfSpins=0;
-var numberOfMatches;
+var population=70; //pool of numbers to select from
+var computerDraw=20; //count of numbers the computer draws
+var humanDraw=10; //count of numbers the player draws
+var numberOfDraws=0; //number of times to perform the draw
+var numberOfMatches; //mumber of matches between the player's draw and the computer's draw
+var sumOfMatches=0; //sum of the 'number of matches' (i.e. 3 draws.  1st draw 5 are the same, 2nd draw 2 are the same and 3rd draw 0 are the same.  sumOfMatches = 7)
 
-var drawMin=0;
-var drawMax=0;
-var drawMean=0;
-var drawMedian=0;
-var drawMode=0;
+var drawMin=0; //minium value in the histogram
+var drawMax=0; //maximum value in the histogram
+var drawMean=0; //mean value in the histogram
+var drawMedian=0; //median value in the histogram
+var drawMode=0; //mode value in the histogram
+var highestNumber=0; //stores the highest number of the matchArray object
 
 var computerNumbers=new Array(); //stores the random numbers for the computer
 var humanNumbers=new Array(); //stores the random numbers for the player
 var mmmmmArray=new Array(); //stores the values for the 5 m's
+var medianArray=new Array(); //stores the number of matches per draw as a record
 var matchArray=[0,0,0,0,0,0,0,0,0,0,0]; //stores the values for the match array (number/frequency)
 
 //generates random data (for computer)
@@ -54,14 +56,14 @@ function multipleDraws()
 {
 	//local variables
 	var x;
-	numberOfSpins=document.getElementById('numberOfSpins_id').value;
+	numberOfDraws=document.getElementById('numberOfDraws_id').value;
 
 	//form validation
-	if(numberOfSpins > 1000 || numberOfSpins < 1)
+	if(numberOfDraws > 1000 || numberOfDraws < 1)
 	{
 		alert("please keep the number between 1 and 1000 for now.")
-		numberOfSpins=0;
-		document.getElementById('numberOfSpins_id').value=0;
+		numberOfDraws=0;
+		document.getElementById('numberOfDraws_id').value=0;
 	}
 	else
 	{
@@ -74,16 +76,13 @@ function multipleDraws()
 	document.getElementById('btn_render_id').disabled=true;
 
 	//draw the random sets of numbers for the number of times specified
-	for(x=0; x<numberOfSpins; x++)
+	for(x=0; x<numberOfDraws; x++)
 	{
 		randomizeComputerAndHuman();
 	}
 
 	document.getElementById('btn_draw_id').disabled=false;
 	document.getElementById('btn_render_id').disabled=false;
-
-	//call the MMMMM method(s)
-
 }
 
 //finds the number of matches between the computer's numbers and the player's numbers
@@ -105,27 +104,89 @@ function findMatches()
 			}
 		}
 	}
-	indexToUpdate=numberOfMatches;
-	matchArray[indexToUpdate] += 1;
-	//used for testing purposes
-	//alert("number of matches: " + numberOfMatches);
+	indexToUpdate=numberOfMatches; //set the index to update (how many numbers the player and the computer had the same)
+	matchArray[indexToUpdate] += 1; //update the index (number of matches) by 1
+
+	sumOfMatches=indexToUpdate+sumOfMatches; //update the sumOfMatches for Mean
+
+	medianArray.push(indexToUpdate); //update the medianArray with the newest entry (number of matches from the draw)
+
 }
 
 //calculates the MMMMM
 function calculateMMMMM()
 {
-	var x;
+	var x=0;
+	var y=1;
+	var z;
+	var medianOne;
+	var medianTwo;
+	var drawMedianEven;
+	var drawMedianOdd;
 	//humanNumbers.sort(function(a, b){return a - b});
 	//computerNumbers.sort(function(a, b){return a - b});
 	//matchArray.sort(function(a, b){return a - b});
-	alert(matchArray);
+	//alert("matchArray length is: " + matchArray.length);
+
 	//min
 	while(matchArray[x]==0)
 	{
 		x++;
 	}
-	drawMin=matchArray[x];
+	drawMin=x;
+
+	//max
+	while(matchArray[matchArray.length-y]==0)
+	{
+		y++;
+	}
+	drawMax=11-(y);
+
+	//mean
+	drawMean=(sumOfMatches/numberOfDraws);
+
+	//median
+	medianArray.sort(function(a, b){return a - b});
+	if(medianArray.length % 2==0)
+	{
+		medianOne=(medianArray.length/2);
+		medianTwo=medianOne+1;
+		drawMedianEven=medianArray[medianOne]+medianArray[medianTwo];
+		drawMedian=drawMedianEven/2;
+	}
+	else
+	{
+		drawMedianOdd=((medianArray.length-1)/2)+1;
+		drawMedian=medianArray[drawMedianOdd];
+	}
+
+	//mode
+	for(z=0; z<matchArray.length; z++)
+	{
+		if(highestNumber<matchArray[z])
+		{
+			highestNumber=matchArray[z];
+			drawMode=matchArray.indexOf(matchArray[z]);
+		}
+		else
+		{
+			//keep the current value
+		}
+	}
+
+	//call the alert method
 	alertMMMMM();	
+}
+
+//alerts both the computer and player arrays
+function alertMMMMM()
+{
+	alert("Min, Max, Mean, Median and Mode!\n" 
+	+ "Min: " + drawMin + "\n"
+	+ "Max: " + drawMax + "\n"
+	+ "Mean: " + drawMean + "\n"
+	+ "Median: " + drawMedian + "\n"
+	+ "Mode: " + drawMode);
 }
 
 //alerts both the computer and player arrays
@@ -135,14 +196,6 @@ function alertPcHuman()
 	+ "Computer Numbers: " + computerNumbers.toString() + "\n"
 	+ "Player Numbers: " + humanNumbers.toString() + "\n"
 	+ "Number of matches: " + numberOfMatches);
-}
-
-//alerts both the computer and player arrays
-function alertMMMMM()
-{
-	alert("Min, Max, Mean, Median and Mode!\n" 
-	+ "Min: " + drawMin + "\n"
-	+ "Sorted: " + humanNumbers);
 }
 
 //returns the computer's array to the GUI call
@@ -161,4 +214,26 @@ function getHuman()
 function getMatch()
 {
 	return matchArray;
+}
+
+//deletes all the data in memory
+function deleteData()
+{
+	numberOfDraws=0; //number of times to perform the draw
+	numberOfMatches=0; //mumber of matches between the player's draw and the computer's draw
+	sumOfMatches=0; //sum of the 'number of matches' (i.e. 3 draws.  1st draw 5 are the same, 2nd draw 2 are the same and 3rd draw 0 are the same.  sumOfMatches = 7)
+
+	drawMin=0; //minium value in the histogram
+	drawMax=0; //maximum value in the histogram
+	drawMean=0; //mean value in the histogram
+	drawMedian=0; //median value in the histogram
+	drawMode=0; //mode value in the histogram
+
+	computerNumbers=new Array(); //stores the random numbers for the computer
+	humanNumbers=new Array(); //stores the random numbers for the player
+	mmmmmArray=new Array(); //stores the values for the 5 m's
+	medianArray=new Array(); //stores the number of matches per draw as a record
+	matchArray=[0,0,0,0,0,0,0,0,0,0,0]; //stores the values for the match array (number/frequency)
+
+	document.getElementById('numberOfDraws_id').value=0; //set the value on the form to 0
 }
